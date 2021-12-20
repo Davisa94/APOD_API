4d438eb051d0d80ada67f525b4ed87510b34d473
Davisa94 <Austin.i.davis94@gmail.com>
2021-12-20 15:48:48 -0800

    Switched to using mysql 2 package; Fixed syntax errors.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const host = "localhost";
 const port = 7878;
 const secrets = require("./secrets.js");
@@ -9,7 +9,7 @@ const DBpassword = secrets.DBpassword;
 const app = express();
 
 const DBconnection = mysql.createConnection({
-   host: dbHost,
+   host: DBhost,
    user: DBuser,
    password: DBpassword
 });
@@ -20,7 +20,13 @@ DBconnection.connect(function(err){
    console.log("Succesfully connected to the database");
 })
 
+function defaultGet(req, res)
+{
+   console.log(`Welcome to the NASA APOD Review system use /instructions to learn more.`);
+}
 
-server.listen(port, host, () =>{
+app.get('/', defaultGet)
+
+app.listen(port, () =>{
    console.log(`server is running @ http://${host}:${port}`);
 });
