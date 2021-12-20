28d9e205f15347e81d7aeb8080eafd77968405e0
Davisa94 <Austin.i.davis94@gmail.com>
2021-12-19 20:21:09 -0800

    added space to validate comment

--- Design-Docs/buildDatabase.sql
+++ Design-Docs/buildDatabase.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS NASA_PICTURE_RATING_SYSTEM;
 USE NASA_PICTURE_RATING_SYSTEM;
 
---User table for storing the relation between user ID and email:
+-- User table for storing the relation between user ID and email:
 CREATE TABLE user (
    user_id SERIAL NOT NULL,
    user_email VARCHAR(255) NOT NULL,
