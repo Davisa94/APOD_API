8fbe75ea8966d88f103b7cd7869fb05d58a6fbae
Davisa94 <Austin.i.davis94@gmail.com>
2021-12-19 20:59:16 -0800

    begin error checking schema;

--- Design-Docs/buildDatabase.sql
+++ Design-Docs/buildDatabase.sql
@@ -14,5 +14,17 @@ CREATE TABLE `picture` (
    `picture_id` SERIAL NOT NULL,
    `picture_URI` VARCHAR(255) NOT NULL,
    `date_posted` DATE DEFAULT(curdate()),
-   PRIMARY KEY (picture_id)
+   PRIMARY KEY (`picture_id`)
+);
+
+-- Ratings table to store ratings in the relation to the user and the picture
+CREATE TABLE `rating` (
+   `rating_id` SERIAL NOT NULL,
+   `rating_value` ENUM(1,2,3,4,5),
+   `user_id` INT NOT NULL,
+   CONSTRAINT `user_rating`
+      FOREIGN KEY (`user_id`)
+      REFERENCES `user`(`user_id`)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
 );
