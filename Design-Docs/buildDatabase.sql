a73b9710b0e571fa33410b8b0952454cd58222cd
Davisa94 <Austin.i.davis94@gmail.com>
2021-12-19 20:41:21 -0800

    fixed a few minor syntax errors

--- Design-Docs/buildDatabase.sql
+++ Design-Docs/buildDatabase.sql
@@ -2,18 +2,19 @@ CREATE DATABASE IF NOT EXISTS NASA_PICTURE_RATING_SYSTEM;
 USE NASA_PICTURE_RATING_SYSTEM;
 
 -- User table for storing the relation between user ID and email:
-CREATE TABLE user (
-   user_id SERIAL NOT NULL,
-   user_email VARCHAR(255) NOT NULL,
-   PRIMARY KEY (user_id),
-   UNIQUE(user_id),
-   UNIQUE(user_email)
+CREATE TABLE `user` (
+   `user_id` SERIAL NOT NULL,
+   `user_email` VARCHAR(255) NOT NULL,
+   PRIMARY KEY (`user_id`),
+   UNIQUE(`user_id`),
+   UNIQUE(`user_email`)
 );
 
 -- Picture Table containing relation between image URI , date posted, and the ID
-CREATE TABLE picture (
-   picture_id = SERIAL NOT NUL;
-   picture_URI VARCHAR(255) NOT NULL,
-   date_posted DATE DEFAULT(CURRENT_DATE),
-   PRIMARY KEY
-)
+CREATE TABLE `picture` (
+   `picture_id` SERIAL NOT NULL,
+   `picture_URI` VARCHAR(255) NOT NULL,
+   `date_posted` DATE DEFAULT(curdate()),
+   PRIMARY KEY (picture_id),
+   UNIQUE(picture_id),
+);
