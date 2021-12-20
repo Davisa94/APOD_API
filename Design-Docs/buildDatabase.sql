ad8122c8f6f82ed201d4d12fa11e3d6dbd7343e2
Davisa94 <Austin.i.davis94@gmail.com>
2021-12-19 20:17:39 -0800

    Begin writing testing tools for sql server

--- Design-Docs/buildDatabase.sql
+++ Design-Docs/buildDatabase.sql
@@ -1,4 +1,13 @@
 CREATE DATABASE IF NOT EXISTS NASA_PICTURE_RATING_SYSTEM;
 USE NASA_PICTURE_RATING_SYSTEM;
 
+--User table for storing the relation between user ID and email:
+CREATE TABLE user (
+   user_id SERIAL NOT NULL,
+   user_email VARCHAR(255) NOT NULL,
+   PRIMARY KEY (user_id),
+   UNIQUE(user_id),
+   UNIQUE(user_email)
+);
+
 --
