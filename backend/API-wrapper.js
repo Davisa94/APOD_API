84d341087a20787628e9d996adde5a63ac24ab68
Davisa94 <Austin.i.davis94@gmail.com>
2021-12-21 01:46:00 -0800

    Finished API Wrapper

--- backend/API-wrapper.js
+++ backend/API-wrapper.js
@@ -5,14 +5,15 @@
 // improvements.
 //////////////////////////////////////
 const fetcher = require("node-fetch");
-const APOD_API_URL = "https://api.nasa.gov/planetary/apod?api_key=";
+const APOD_API_URL = "https://api.nasa.gov/planetary/apod";
 
 
 /*************************************
  * GET the latest picture and return it.
+ * @param String key
  *************************************/
-function getLatestPicture(){
-   const url = `${APOD_API_URL}${key}`;
+function getLatestPicture(key){
+   const url = `${APOD_API_URL}?api_key=${key}`;
    const options = {
       "method": "GET",
    };
@@ -24,9 +25,10 @@ function getLatestPicture(){
 
 /*************************************
  * @param Date date
+ * @param String key
  ************************************/
 function getPictureByDate(date, key){
-   const url = `${APOD_API_URL}${key}`;
+   const url = `${APOD_API_URL}?api_key=${key}?date=${date}`;
    const options = {
       "method": "GET",
    };
@@ -34,3 +36,6 @@ function getPictureByDate(date, key){
    console.debug(reply);
    return reply;
 }
+
+module.exports.getLatestPicture = getLatestPicture;
+module.exports.getPictureByDate = getPictureByDate;
--- backend/secrets-template.js
+++ backend/secrets-template.js
@@ -2,4 +2,4 @@ module.exports.DBhost = "";
 module.exports.DBuser = "";
 module.exports.DBpassword = "";
 module.exports.DBschema = "nasa_picture_rating_system";
-module.exports.API_key = "ENTER YOUR API KEY HERE";
+module.exports.API_key = "DEMO_KEY";
