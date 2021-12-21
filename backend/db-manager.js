fc15ef3a69fd58de164ea5ee71d0828210acc76e
Davisa94 <Austin.i.davis94@gmail.com>
2021-12-20 20:06:08 -0800

    Comment formatting

--- backend/db-manager.js
+++ backend/db-manager.js
@@ -25,10 +25,13 @@ async function queryPictureByDate(date, connection){
    );
 }
 
-// takes in two dates and a connection object
-// TODO: Ensure that the expected date format is specified here. 
-// returns ALL Columns for the Picture table with the matching date
-// range
+/********************************************************
+   takes in two dates and a connection object
+   TODO: Ensure that the expected date format is specified 
+   here. 
+   Returns ALL Columns for the Picture table with
+   the matching date range
+*********************************************************/
 async function queryPictureByDateRange(startDate, endDate, connection){
    var response = await connection.promise().query(
       `SELECT * \
@@ -37,31 +40,49 @@ async function queryPictureByDateRange(startDate, endDate, connection){
    );
 }
 
+/********************************************************
+   We assume that email validation has already occured
+*********************************************************/
 async function addUser(email, connection){
    var response = await connection.promise().query(
       ``
    );
 }
 
+/********************************************************
+   We assume that email validation has already occured
+*********************************************************/
 async function updateUser(oldEmail, newEmail, connection){
    var response = await connection.promise().query(
       ``
    );
 }
 
+/********************************************************
+   We assume that email validation has already occured
+*********************************************************/
 async function deleteUser(email, connection){
    var response = await connection.promise().query(
       ``
    );
 }
 
-
+/********************************************************
+   We assume that validation has already occured,
+   the database will reject invalid ratings 
+   (below 1 or above 5)
+*********************************************************/
 async function addRating(pictureId, email, connection){
    var response = await connection.promise().query(
       ``
    );
 }
 
+/********************************************************
+   We assume that validation has already occured,
+   the database will reject invalid ratings 
+   (below 1 or above 5)
+*********************************************************/
 async function updateRating(pictureId, email, connection){
    var response = await connection.promise().query(
       ``
