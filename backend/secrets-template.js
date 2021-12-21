e3774a2b53814dac386f0b4f561585736a75a119
Davisa94 <Austin.i.davis94@gmail.com>
2021-12-20 16:06:40 -0800

    updated template to match secrets file (minus secrets)

--- backend/secrets-template.js
+++ backend/secrets-template.js
@@ -1,3 +1,4 @@
-const DBhost = "";
-const DBuser = "";
-const DBpassword = ""; 
+module.exports.DBhost = "";
+module.exports.DBuser = "";
+module.exports.DBpassword = "";
+module.exports.DBschema = "nasa_picture_rating_system";
