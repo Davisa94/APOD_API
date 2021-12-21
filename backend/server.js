2dbc43f8603826fd4693dcf192f1cb21eee27eaf
Davisa94 <Austin.i.davis94@gmail.com>
2021-12-20 22:01:56 -0800

    Updated server to use bodyparser middleware

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require("body-parser")
+const router = express.Router();
 const mysql = require("mysql2");
 const host = "localhost";
 const port = 7878;
@@ -30,13 +32,29 @@ function defaultGet(req, res)
 {
    console.log(`Welcome to the NASA APOD Review system use /instructions to learn more.`);
 }
-
-app.get('/', defaultGet)
-app.get('/userRatings', async (req, res) => {
+// setup body-parser
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+// use router
+router.get('/userRatings', async (req, res) => {
    const queryResponse = await DBinteractor.getRatingsByEmail(req.query.email, DBconnection);
    console.log(req.query);
    res.json(queryResponse);
 });
+
+// app.get('/', defaultGet)
+// app.get('/userRatings', async (req, res) => {
+//    const queryResponse = await DBinteractor.getRatingsByEmail(req.query.email, DBconnection);
+//    console.log(req.query);
+//    res.json(queryResponse);
+// });
+// app.post('/userRating', async (req, res) => {
+//    var rating = req.body;
+//    console.log(req.body);
+//    // const queryResponse = await DBinteractor.setRating()
+// })
+
+app.use('/', router);
 
 app.listen(port, () =>{
    console.log(`server is running @ http://${host}:${port}`);
