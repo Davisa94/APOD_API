62c3a5f90454a64070bd83611fb00595f79cad3f
Davisa94 <Austin.i.davis94@gmail.com>
2021-12-20 19:39:55 -0800

    Built out database queries for getting 3 of the endpoints

--- Design-Docs/exampleQueries.sql
+++ Design-Docs/exampleQueries.sql
@@ -3,5 +3,16 @@ SELECT U.user_email, P.picture_URI, R.rating_value
 FROM rating R
 LEFT JOIN user U on r.user_id = u.user_id
 LEFT JOIN picture p on r.picture_id = p.picture_id
-WHERE user_email like "Melons@melonmail.com"
+WHERE user_email like "Melons@melonmail.com";
 
+-- get a single picture by date
+
+SELECT *
+FROM picture
+WHERE date_posted = '2020-11-21';
+
+
+-- select all pictures in a date range
+SELECT *
+FROM picture
+WHERE (date_posted BETWEEN '2020-11-15' AND '2020-11-25'); 
--- backend/db-manager.js
+++ backend/db-manager.js
@@ -13,4 +13,32 @@ async function queryAllRatingsByEmail(userEmail, connection){
    console.log(response[0]);
    return response[0];
 }
-module.exports.test = queryAllRatingsByEmail;
+
+// takes in a date and a connection object
+// TODO: Ensure that the expected date format is specified here. 
+// returns ALL Columns for the Picture table with the matching date
+async function queryPictureByDate(date, connection){
+   var response = await connection.promise().query(
+      `SELECT * \
+      FROM picture \
+      WHERE date_posted = "${date}";`
+   )
+}
+
+// takes in two dates and a connection object
+// TODO: Ensure that the expected date format is specified here. 
+// returns ALL Columns for the Picture table with the matching date
+// range
+async function queryPictureByDateRange(startDate, endDate, connection){
+   var response = await connection.promise().query(
+      `SELECT * \
+      FROM picture \
+      WHERE (date_posted BETWEEN "${startDate}" AND "${endDate}"); `
+   )
+}
+
+
+// Exports
+module.exports.getRatingsByEmail = queryAllRatingsByEmail;
+module.exports.getPictureByDate = queryPictureByDate;
+module.exports.getPictureByDateRange = queryPictureByDateRange;
